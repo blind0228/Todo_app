0e083a3a1aaf1b242783a8d793c392f5d2468a3c
JEONG <prophet000228@gmail.com>
2025-05-11 21:27:48 +0900

    [backend] DELETE /todos/:id API 구현

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,13 @@ app.post('/todos', (req, res) => {
     res.status(201).json(newTodo);
   });
 
+app.delete('/todos/:id', (req, res) => {
+const { id } = req.params;
+todos = todos.filter(todo => todo.id !== Number(id));
+res.status(204).send(); // 삭제 성공, 반환할 데이터 없음
+});
+  
+
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중`);
 });
